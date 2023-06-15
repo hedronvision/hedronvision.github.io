0a797dca63e1c04dbe21439d7e41442d5f9d3f1a
Sam Redmond <contact+github@sredmond.io>
2023-06-15 14:41:46 -0700

    Update to the latest version of Eventually.
    At some point in the past several years, HTML5 UP [1] updated their Eventually
    template [2] - so this commit brings us back to the latest.
    
    [1]: https://html5up.net/
    [2]: https://html5up.net/eventually

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,8 @@
 					// Images (in the format of 'url': 'alignment').
 						images: {
 							'images/bg01.jpg': 'center',
-//							'images/bg02.jpg': 'center',
-//							'images/bg03.jpg': 'center'
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
@@ -104,67 +104,67 @@
 	// Signup Form.
 		(function() {
 
-			// // Vars.
-			// 	var $form = document.querySelectorAll('#signup-form')[0],
-			// 		$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-			// 		$message;
+			// Vars.
+				var $form = document.querySelectorAll('#signup-form')[0],
+					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+					$message;
 
-			// // Bail if addEventListener isn't supported.
-			// 	if (!('addEventListener' in $form))
-			// 		return;
+			// Bail if addEventListener isn't supported.
+				if (!('addEventListener' in $form))
+					return;
 
-			// // Message.
-			// 	$message = document.createElement('span');
-			// 		$message.classList.add('message');
-			// 		$form.appendChild($message);
+			// Message.
+				$message = document.createElement('span');
+					$message.classList.add('message');
+					$form.appendChild($message);
 
-			// 	$message._show = function(type, text) {
+				$message._show = function(type, text) {
 
-			// 		$message.innerHTML = text;
-			// 		$message.classList.add(type);
-			// 		$message.classList.add('visible');
+					$message.innerHTML = text;
+					$message.classList.add(type);
+					$message.classList.add('visible');
 
-			// 		window.setTimeout(function() {
-			// 			$message._hide();
-			// 		}, 3000);
+					window.setTimeout(function() {
+						$message._hide();
+					}, 3000);
 
-			// 	};
+				};
 
-			// 	$message._hide = function() {
-			// 		$message.classList.remove('visible');
-			// 	};
+				$message._hide = function() {
+					$message.classList.remove('visible');
+				};
 
-			// // Events.
-			// // Note: If you're *not* using AJAX, get rid of this event listener.
-			// 	$form.addEventListener('submit', function(event) {
+			// Events.
+			// Note: If you're *not* using AJAX, get rid of this event listener.
+				$form.addEventListener('submit', function(event) {
 
-			// 		event.stopPropagation();
-			// 		event.preventDefault();
+					event.stopPropagation();
+					event.preventDefault();
 
-			// 		// Hide message.
-			// 			$message._hide();
+					// Hide message.
+						$message._hide();
 
-			// 		// Disable submit.
-			// 			$submit.disabled = true;
+					// Disable submit.
+						$submit.disabled = true;
 
-			// 		// Process form.
-			// 		// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-			// 		// but there's enough here to piece together a working AJAX submission call that does.
-			// 			window.setTimeout(function() {
+					// Process form.
+					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+					// but there's enough here to piece together a working AJAX submission call that does.
+						window.setTimeout(function() {
 
-			// 				// Reset form.
-			// 					$form.reset();
+							// Reset form.
+								$form.reset();
 
-			// 				// Enable submit.
-			// 					$submit.disabled = false;
+							// Enable submit.
+								$submit.disabled = false;
 
-			// 				// Show message.
-			// 					// $message._show('success', 'Thank you!');
-			// 					$message._show('failure', 'Whoops! We\'re still building this feature. Please try again later.');
+							// Show message.
+								$message._show('success', 'Thank you!');
+								//$message._show('failure', 'Something went wrong. Please try again.');
 
-			// 			}, 750);
+						}, 750);
 
-			// 	});
+				});
 
 		})();
 
